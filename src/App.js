eb25d78a45a8005f309a9a94ea84b8ea6a78a0e1
burak <mbyilmazer38@gmail.com>
2022-06-30 22:51:21 +0300

    1

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import LifeCycleMethods from './components/LifeCycleMethods';
+import LifeCycleMethods from "./components/LifeCycleMethods";
+import { useState } from "react";
+import UseEffectHook from "./components/UseEffectHook";
+import User from "./components/User";
 
 function App() {
+  // const [show, setShow] = useState(true);
+
   return (
     <div className="container text-center mt-4">
-      <LifeCycleMethods />
+      {/* <button className="btn btn-dark" onClick={() => setShow(!show)}>
+        {show ? "Hide" : "Show"}
+      </button> */}
+      {/* {show && <LifeCycleMethods />} */}
+      {/* {show && <UseEffectHook />} */}
+      <User />
     </div>
   );
 }
